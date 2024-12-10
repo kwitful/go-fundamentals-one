fee4c10c1db0fbbabd00d3586ccd5c69ebde7390
kwitful <119952091+kwitful@users.noreply.github.com>
2024-12-11 01:55:45 +0300

    updated

---
+++ part2-basicDataTypes.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Booleans
+	var isForSale bool = true
+	var isInStock bool = false
+
+	fmt.Println(isForSale, isInStock)
+
+	// Integers
+	var carNumber int32 = 976545655
+	var carLarger int64 = 9650846909004596
+
+	fmt.Println(carNumber, carLarger)
+
+	// Floats
+
+	var carMPG float32 = 43.72
+	var carMiles float64 = 646465.2342
+
+	fmt.Println(carMPG, carMiles)
+
+	// Strings
+
+	var carName string = "Corolla"
+
+	fmt.Println(carName)
+
+}
