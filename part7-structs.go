08d1fe8d25d7bc21d63edc2065c3213b2ebcf22f
kwitful <119952091+kwitful@users.noreply.github.com>
2024-12-11 19:02:41 +0300

    Update part7-structs.go

--- part7-structs.go
+++ part7-structs.go
@@ -9,6 +9,11 @@ type Car struct {
 	model int
 }
 
+type Factory struct {
+	name    string
+	product Car // Structs can containt other structs as fields
+}
+
 func main() {
 	// Initializing a struct
 	bmw := Car{brand: "BMW", model: 1994}
@@ -16,4 +21,16 @@ func main() {
 	// Printing struct fields
 	fmt.Println("Car Brand:", bmw.brand)
 	fmt.Println("Model:", bmw.model)
+
+	// You can modify fields later on
+	bmw.model = 1995
+	fmt.Println("Updated model:", bmw.model)
+
+	// NESTED STRUCTS
+
+	factoryOne := Factory{name: "Factory Alpha", product: Car{brand: "Mercedes", model: 1998}}
+
+	fmt.Println("The name of the factory is:", factoryOne.name)
+	fmt.Println("The main product of the factory is:", factoryOne.product.brand) // You can access sub-fields of nested structs like this
+	fmt.Println("The model of the factory's products:", factoryOne.product.model)
 }
